1725215a9d04cf6de78411ea2389ad9047a57dde
Justyna Zygmunt <justynazygmunt@Justynas-MacBook-Air.local>
2018-06-27 18:56:20 +0100

    Feature: use Board from JAR file in the MainAction.

--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MainActivity.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MainActivity.java
@@ -2,12 +2,64 @@ package com.justynaapps.justynazygmunt.tictactoe;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+
+import com.core.tictactoe.Board;
 
 public class MainActivity extends AppCompatActivity {
+
+
+    private Board board;
+    String currentSign;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        board = new Board(3);
+        currentSign = "X";
+
+        for (int i = 1; i < board.getPlaces().length +1; i++ ){
+            final TextView singleGrid = getTextViewForId(i);
+            singleGrid.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    if (singleGrid.getText() != "X" && singleGrid.getText() != "O" && !board.isWon()) {
+                        int numberPickedByUser = Integer.parseInt(singleGrid.getText().toString());
+                        board.putSignOnBoard(currentSign, numberPickedByUser);
+                        refreshSingleGrid(currentSign, numberPickedByUser);
+                        changeCurrentSign();
+                    }
+                }
+            });
+        }
+        renderGridWithNumbers();
+    }
+
+    private TextView getTextViewForId(int i) {
+        int idOfWindowInMobileGrid = getResources().getIdentifier("place_number_" + i, "id", getPackageName());
+        return findViewById(idOfWindowInMobileGrid);
+    }
+
+    private void renderGridWithNumbers() {
+        for (int i = 1; i < board.getPlaces().length +1 ; i++) {
+            final TextView placeOnMobileGrid = getTextViewForId(i);
+            placeOnMobileGrid.setText(board.valueAtIndex(i-1));
+        }
+    }
+
+    private void refreshSingleGrid(String currentSign, int position) {
+        final TextView placeOnMobileGrid = getTextViewForId(position);
+        placeOnMobileGrid.setText(currentSign);
+    }
+
+    private void changeCurrentSign() {
+        if (currentSign.equals("X")) {
+            currentSign = "O";
+        } else {
+            currentSign = "X";
+        }
     }
 }
