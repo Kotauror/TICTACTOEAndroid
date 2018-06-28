069c2c1c664c5e731ed4351167cd90d08b6952e8
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-06-28 14:15:41 +0100

    Test: Setup espresso testing framework, AppCOntext test is passing.

--- app/src/androidTest/java/com/justynaapps/justynazygmunt/tictactoe/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/justynaapps/justynazygmunt/tictactoe/ExampleInstrumentedTest.java
@@ -2,12 +2,14 @@ package com.justynaapps.justynazygmunt.tictactoe;
 
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 /**
  * Instrumented test, which will execute on an Android device.
@@ -16,11 +18,17 @@ import static org.junit.Assert.*;
  */
 @RunWith(AndroidJUnit4.class)
 public class ExampleInstrumentedTest {
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityRule =
+            new ActivityTestRule(MainActivity.class);
+
     @Test
     public void useAppContext() {
-        // Context of the app under test.
         Context appContext = InstrumentationRegistry.getTargetContext();
 
         assertEquals("com.justynaapps.justynazygmunt.tictactoe", appContext.getPackageName());
     }
 }
+
+
