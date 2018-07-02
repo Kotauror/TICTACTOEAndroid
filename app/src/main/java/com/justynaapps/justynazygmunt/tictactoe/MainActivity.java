cfbd78517ce7117b2107545fa44a718040ab5516
Justyna Zygmunt <justynazygmunt@Justynas-MacBook-Air.local>
2018-07-02 18:25:47 +0100

    Refactor: Listener set in the xml.

--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MainActivity.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MainActivity.java
@@ -2,6 +2,7 @@ package com.justynaapps.justynazygmunt.tictactoe;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.TextView;
 
 import com.core.tictactoe.Board;
@@ -18,26 +19,16 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         createGame();
-        createMoveListenersForFieldsOnGrid();
-
         renderBoard();
     }
 
     private void createGame() {
         Board board = new Board(3);
         MobilePlayer player1 = new MobilePlayer("X");
-//        MobilePlayer player2 = new MobilePlayer("O");
+        MobilePlayer player2 = new MobilePlayer("O");
         Renderer renderer = new Renderer();
         ComputerPlayer computerPlayer = new ComputerPlayer("O");
         this.mobileGame = new MobileGame(board, player1, computerPlayer, renderer);
-    }
-
-    private void createMoveListenersForFieldsOnGrid() {
-        for (int cellNumber = 1; cellNumber < mobileGame.getBoard().getPlaces().length +1; cellNumber++ ){
-            final TextView singleGrid = getTextViewForId(cellNumber);
-            MoveListener moveListener = new MoveListener(getApplicationContext(), singleGrid, cellNumber, mobileGame);
-            singleGrid.setOnClickListener(moveListener);
-        }
     }
 
     protected MobileGame getMobileGame() {
@@ -54,5 +45,20 @@ public class MainActivity extends AppCompatActivity {
             final TextView placeOnMobileGrid = getTextViewForId(i);
             placeOnMobileGrid.setText(getMobileGame().getBoard().valueAtIndex(i-1));
         }
+    }
+
+    public void handleSpace(View view) {
+        String cellPickedByUser = ((TextView)view).getText().toString();
+        if (this.mobileGame.getBoard().isNonTaken(cellPickedByUser) && !this.mobileGame.getBoard().isWon()) {
+            this.mobileGame.playMove(Integer.parseInt(cellPickedByUser));
+//            renderBoard();
+            updateView(view, Integer.parseInt(cellPickedByUser));
+            mobileGame.afterClick();
+        }
+    }
+
+    private void updateView(View view, int numberPickedByUser) {
+        ((TextView)view).setText(this.mobileGame.getBoard().valueAtIndex(numberPickedByUser-1));
+        view.setOnClickListener(null);
     }
 }
--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MoveListener.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MoveListener.java
@@ -1,42 +1,42 @@
-package com.justynaapps.justynazygmunt.tictactoe;
-
-import android.content.Context;
-import android.view.View;
-import android.widget.TextView;
-
-public class MoveListener implements View.OnClickListener {
-
-    private TextView singleGrid;
-    private int cellNumber;
-    private MobileGame mobileGame;
-    private Context context;
-
-    public MoveListener(Context context, TextView singleGrid, int cellNumber, MobileGame mobileGame) {
-        this.context = context;
-        this.singleGrid = singleGrid;
-        this.cellNumber = cellNumber;
-        this.mobileGame = mobileGame;
-    }
-
-    @Override
-    public void onClick(View view) {
-        if (this.mobileGame.getBoard().isNonTaken(Integer.toString(cellNumber)) && !this.mobileGame.getBoard().isWon()) {
-            int numberPickedByUser = Integer.parseInt(this.singleGrid.getText().toString());
-            this.mobileGame.playMove(numberPickedByUser);
-            updateView(numberPickedByUser);
-            mobileGame.afterClick();
-            // 2. Messanger.InformOfMove(numberPickedByUser, mobileGame.getPassivePlayer().getSign();)
-
-            // showToast(numberPickedByUser);
-        }
-    }
-
-    private void updateView(int numberPickedByUser) {
-        this.singleGrid.setText(this.mobileGame.getBoard().valueAtIndex(numberPickedByUser-1));
-    }
-
-//    private void showToast(int numberPickedByUser) {
-//        String sign = mobileGame.getPassivePlayer().getSign();
-//        Toast.makeText(context, "Player " + sign + " picked " + numberPickedByUser, Toast.LENGTH_LONG).show();
+//package com.justynaapps.justynazygmunt.tictactoe;
+//
+//import android.content.Context;
+//import android.view.View;
+//import android.widget.TextView;
+//
+//public class MoveListener implements View.OnClickListener {
+//
+//    private TextView singleGrid;
+//    private int cellNumber;
+//    private MobileGame mobileGame;
+//    private Context context;
+//
+//    public MoveListener(Context context, TextView singleGrid, int cellNumber, MobileGame mobileGame) {
+//        this.context = context;
+//        this.singleGrid = singleGrid;
+//        this.cellNumber = cellNumber;
+//        this.mobileGame = mobileGame;
 //    }
-}
+//
+//    @Override
+//    public void onClick(View view) {
+//        if (this.mobileGame.getBoard().isNonTaken(Integer.toString(cellNumber)) && !this.mobileGame.getBoard().isWon()) {
+//            int numberPickedByUser = Integer.parseInt(this.singleGrid.getText().toString());
+//            this.mobileGame.playMove(numberPickedByUser);
+//            updateView(numberPickedByUser);
+//            mobileGame.afterClick();
+//            // 2. Messanger.InformOfMove(numberPickedByUser, mobileGame.getPassivePlayer().getSign();)
+//
+//            // showToast(numberPickedByUser);
+//        }
+//    }
+//
+//    private void updateView(int numberPickedByUser) {
+//        this.singleGrid.setText(this.mobileGame.getBoard().valueAtIndex(numberPickedByUser-1));
+//    }
+//
+////    private void showToast(int numberPickedByUser) {
+////        String sign = mobileGame.getPassivePlayer().getSign();
+////        Toast.makeText(context, "Player " + sign + " picked " + numberPickedByUser, Toast.LENGTH_LONG).show();
+////    }
+//}
