92db9a92a704f416cafa950c742918720da77156
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-02 16:27:42 +0100

    Fix & Refactor: reimport JAR file after changes made to the original code.

--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MobileGame.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/MobileGame.java
@@ -10,18 +10,18 @@ public class MobileGame extends Game {
     private Renderer renderer;
 
     public MobileGame(Board board, Player playerOne, Player playerTwo, Renderer renderer) {
-        super(null, board, playerOne, playerTwo);
+        super(board, playerOne, playerTwo);
         this.renderer = renderer;
     }
 
     public void playMove(int position) {
-        this.getBoard().putSignOnBoard(this.getActivePlayer().getSign(), position);
+        this.putSingOnBoard(position);
         this.switchPlayers();
     }
 
     public void afterClick() {
         if (this.getActivePlayer() instanceof ComputerPlayer && !this.getBoard().isWon() && !this.getBoard().isTie()) {
-            int computerPosition = getActivePlayer().pickPosition(null, this.getBoard());
+            int computerPosition = getActivePlayer().pickPosition(this.getBoard());
             this.getBoard().putSignOnBoard(this.getActivePlayer().getSign(), computerPosition);
             switchPlayers();
         }
