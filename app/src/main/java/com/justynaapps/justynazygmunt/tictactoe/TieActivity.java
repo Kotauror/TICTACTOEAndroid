42f91793dd6968bb4325e840ca7b7bf45108a408
Justyna Zygmunt <justynazygmunt@Sams-Air.localdomain>
2018-07-04 13:43:03 +0100

    Feature & Test: add play again button to Winner and Tie screens.

--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/TieActivity.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/TieActivity.java
@@ -1,7 +1,9 @@
 package com.justynaapps.justynazygmunt.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class TieActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class TieActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tie);
+    }
+
+    public void moveToNextPagePage(View view) {
+        Intent jumpPage = new Intent(TieActivity.this, WelcomeActivity.class);
+        startActivity(jumpPage);
     }
 }
--- app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/WinnerActivity.java
+++ app/src/main/java/com/justynaapps/justynazygmunt/tictactoe/WinnerActivity.java
@@ -1,7 +1,9 @@
 package com.justynaapps.justynazygmunt.tictactoe;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.TextView;
 
 public class WinnerActivity extends AppCompatActivity {
@@ -20,5 +22,10 @@ public class WinnerActivity extends AppCompatActivity {
         int iDOfPlayerSignTextView = this.getResources().getIdentifier("playerSign", "id", this.getPackageName());
         TextView playerSignTextView = this.findViewById(iDOfPlayerSignTextView);
         playerSignTextView.setText(winnerSign);
+    }
+
+    public void moveToNextPagePage(View view) {
+        Intent jumpPage = new Intent(WinnerActivity.this, WelcomeActivity.class);
+        startActivity(jumpPage);
     }
 }
